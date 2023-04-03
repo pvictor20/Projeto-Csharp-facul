5f3ce4b2e2ff33c5ddfb150ac6aa23ba61d5ba73
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-04-03 20:14:55 -0300

    Laço de repetição

---
+++ LacoRepeticao1/LacoRepeticao1.cs
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.5.33516.290
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "LacoRepeticao1", "LacoRepeticao1\LacoRepeticao1.csproj", "{5DE089F8-BE0E-470A-BB3B-5B0C45E3F30A}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{5DE089F8-BE0E-470A-BB3B-5B0C45E3F30A}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{5DE089F8-BE0E-470A-BB3B-5B0C45E3F30A}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{5DE089F8-BE0E-470A-BB3B-5B0C45E3F30A}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{5DE089F8-BE0E-470A-BB3B-5B0C45E3F30A}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {1E39351D-4154-470D-A624-B604CCBA5638}
+	EndGlobalSection
+EndGlobal
---
+++ LacoRepeticao1/LacoRepeticao1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoRepeticao1
+{
+    internal class Program
+    {
+        static int a, b, c, d;
+
+        static void loop01()
+        {
+            for (int i = 0; i <= 100; i++)//teste no incio
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+            for (int i = 100; i >=0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+        static void loop02() 
+        {
+            a = 0;
+            while(a <=10)//teste no inicio
+            {
+                Console.WriteLine(a);
+                a += 1;
+            }
+            Console.ReadKey();
+
+            b = 0;
+            do
+            {
+                Console.WriteLine(b);
+                b += 1;
+            }
+            while(b <=10);
+        }
+        static void Main(string[] args)
+        {
+            loop01();
+            loop02();
+        }
+    }
+}
